1b2227f51e82d043452e9410a6631cbc462675a5
cpnsing <cpnsingh8181@gmail.com>
2025-01-16 18:27:51 +0530

    hover functionality completed

--- src/components/home/Team.tsx
+++ src/components/home/Team.tsx
@@ -12,7 +12,7 @@ interface TeamMember {
 }
 
 const TeamSection = ({ id }: { id: string }) => {
-  const [selectedMember, setSelectedMember] = useState<TeamMember | null>(null);
+  const [selectedMember, setSelectedMember] = useState("");
 
   const teamMembers: TeamMember[] = [
     {
@@ -54,7 +54,7 @@ const TeamSection = ({ id }: { id: string }) => {
       name: "Sophia Lee",
       role: "Marketing Specialist",
       description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      imgSrc: "/images/member6.png",
+      imgSrc: "/card/micheal.png",
       bgColor: "bg-black",
     },
   ];
@@ -74,22 +74,47 @@ const TeamSection = ({ id }: { id: string }) => {
 
         {/* Team Grid */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-0 mt-10">
-          {teamMembers.slice(0,3).map((member, index) => (
-            <div
+        {teamMembers.slice(0,3).map((member, index) => {
+            return(
+              <>
+              {selectedMember===member?.name ? (
+                <>
+                    <div className="flex cursor-pointer justify-center border border-[#1B429A] items-center rounded-tr-[20px] rounded-br-[20px]  teamCardBg">
+      <div className="max-w-sm w-full  rounded-lg  text-center ">
+        <h2 style={{fontFamily:"Plus Jakarta Sans"}} className="text-white text-[37px] font-bold">{member?.name}</h2>
+        <p style={{fontFamily:"Outfit"}} className="text-[#AEAEAE] text-[26px] mt-1">Founder & CO</p>
+        <p style={{fontFamily:"Geist"}} className="text-white text-[25px] mt-4 text-center font-medium px-6">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        </p>
+        <div className="flex justify-center gap-4 mt-6">
+          <img src='/crypto/facebook.png' alt="facebook" className="w-[38px] h-[38px]" />
+          <img src='/crypto/telegra.png' alt="telegram" className="w-[38px] h-[38px]" />
+           
+         
+          <img src='/crypto/instagram.png' alt="insta" className="w-[38px] h-[38px]" />
+        </div>
+      </div>
+    </div>
+
+                </>
+              ):(
+
+               <div
               key={index}
-              className={`p-6 pt-[70px] pb-[30px] border border-[#1B429A] bg-[#09090B]   transition cursor-pointer ${
+              className={`p-6  pt-[70px] pb-[30px] border border-[#1B429A] bg-[#09090B]  transition cursor-pointer ${
                 index === 0
-                  ? "rounded-tl-[20px] rounded-bl-[20px]" // First item: Top-left & Bottom-left
+                  ? "rounded-tl-[20px] rounded-bl-[20px]" 
                   : index === 2
-                  ? "rounded-tr-[20px] rounded-br-[20px]" // Third item: Top-right & Bottom-right
+                  ? "rounded-tr-[20px] rounded-br-[20px]" 
                   : ""
               }`}
-              onClick={() => setSelectedMember(member)}
+              onMouseEnter={() => setSelectedMember(member?.name)}
+              onMouseLeave={() => setSelectedMember("")}
             >
               <div className="flex flex-col items-center">
                 {/* Image */}
                 <div
-                  className={` rounded-full ${member.bgColor} flex items-center justify-center mb-4`}
+                  className={` rounded-full  flex items-center justify-center mb-4`}
                 >
                   <img src={member.imgSrc} alt={member.name} className="rounded-full w-[234px] h-[234px]" />
                 </div>
@@ -99,18 +124,21 @@ const TeamSection = ({ id }: { id: string }) => {
               </div>
               {/* Button */}
               <div className="mt-4 flex justify-center">
-              <img src="/card/ad.png" className="w-[44px]" />
+                <img src="/card/ad.png" className="w-[44px]" />
               </div>
             </div>
-          ))}
+              )}
+              </>
+            )
+          })}
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-0 mt-1">
           {teamMembers.slice(3,6).map((member, index) => {
             return(
               <>
-              {index===2 ? (
+              {selectedMember===member?.name ? (
                 <>
-                    <div className="flex justify-center border border-[#1B429A] items-center rounded-tr-[20px] rounded-br-[20px]  teamCardBg">
+                    <div className="flex cursor-pointer justify-center border border-[#1B429A] items-center rounded-tr-[20px] rounded-br-[20px]  teamCardBg">
       <div className="max-w-sm w-full  rounded-lg  text-center ">
         <h2 style={{fontFamily:"Plus Jakarta Sans"}} className="text-white text-[37px] font-bold">Nathaniel Lewis</h2>
         <p style={{fontFamily:"Outfit"}} className="text-[#AEAEAE] text-[26px] mt-1">Founder & CO</p>
@@ -139,7 +167,10 @@ const TeamSection = ({ id }: { id: string }) => {
                   ? "rounded-tr-[20px] rounded-br-[20px]" 
                   : ""
               }`}
-              onClick={() => setSelectedMember(member)}
+              onMouseLeave={() => setSelectedMember("")}
+              onMouseEnter={() => {
+                
+                setSelectedMember(member?.name)}}
             >
               <div className="flex flex-col items-center">
                 {/* Image */}
