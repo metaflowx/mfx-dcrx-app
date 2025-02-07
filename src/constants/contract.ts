af89d37335f705a502eb855d2d7027ee3313fd35
mdsac1441 <amdahmed0@gmail.com>
2025-02-07 20:16:52 +0530

    contract change

--- src/constants/contract.ts
+++ src/constants/contract.ts
@@ -2,8 +2,8 @@ import { IcoABI } from "@/app/ABI/IcoABI";
 import { RefferABI } from "@/app/ABI/RewardToken";
 import { TokenABI } from "@/app/ABI/TokenSupply";
 export const TokenContractAddress = "0xB68038cD28202F9fcbc087602923BAcf3Bd1bfE6"; 
-export const ICOContractAddress ="0x34fb5B808d29E75d43947164d7E6120a4Ff37284"
-export const ReferralContractAddress="0x7A5816ABdaE8f26fEBAfE020c720e39a81179948"
+export const ICOContractAddress ="0x863a4D881B31Dad9F3E7e8C04065aD31FB8D25C2"
+export const ReferralContractAddress="0x8B05177a27795Bb453c5AF920023361F7dcBeEF9"
 
 export const contractConfig = {
     address: ReferralContractAddress as `0x${string}`,
